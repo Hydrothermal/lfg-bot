c7dc9e19bdc98f6015d2577e91b669e177a4bc43
cthuluhoop123 <dowzhong@gmail.com>
2018-07-31 19:32:19 +1000

    Generate invite link on start.

--- bot.js
+++ bot.js
@@ -6,24 +6,28 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {
     console.log("Ready.");
+    client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
+        .then(link => {
+            console.log(`Invite link: ${link}`);
+        });
 });
 
 bot.on("message", message => {
-    if(message.author.bot) { return; }
+    if (message.author.bot) { return; }
 
     let args = message.content.split(" ");
     let command = args[0];
 
     args = args.slice(1);
 
-    switch(command) {
+    switch (command) {
         case "!lfg":
-        if(args.length > 0) {
-            message.channel.send(`You are now looking for other players for **${args.join(" ")}**.`);
-        } else {
-            message.channel.send("You must specify a game (`!lfg [game name]`).");
-        }
-        break;
+            if (args.length > 0) {
+                message.channel.send(`You are now looking for other players for **${args.join(" ")}**.`);
+            } else {
+                message.channel.send("You must specify a game (`!lfg [game name]`).");
+            }
+            break;
     }
 });
 
