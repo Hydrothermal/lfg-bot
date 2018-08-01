86810602e4a04937b1bc06aa0c348bc9600ec270
cthuluhoop123 <dowzhong@gmail.com>
2018-08-01 21:24:29 +1000

    fixed dotenv and changed lfg to export object instead of class

--- lfg/lfg.js
+++ lfg/lfg.js
@@ -1,28 +1,26 @@
 const redis = require('redis')
 const client = redis.createClient()
 
-class lfg {
-    static getGames() {
-        return new Promise((resolve, reject) => {
-            client.scan('0', 'MATCH', 'games:*', (err, rep) => {
-                if (err) {
-                    reject(err)
-                }
-                resolve(rep[1].map(game => game.replace(/games:/, '')))
-            })
-        })
-    }
+const lfg = module.exports
 
-    static addGame(name, maxMembers) {
-        return new Promise((resolve, reject) => {
-            client.hmset(`games:${name}`, `queue`, '[]', 'max', maxMembers, (err, reply) => {
-                if (err) {
-                    reject(err)
-                }
-                resolve(reply)
-            })
+lfg.getGames = () => {
+    return new Promise((resolve, reject) => {
+        client.scan('0', 'MATCH', 'games:*', (err, rep) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(rep[1].map(game => game.replace(/games:/, '')))
         })
-    }
+    })
 }
 
-module.exports = lfg
+lfg.addGame = (name, maxMembers) => {
+    return new Promise((resolve, reject) => {
+        client.hmset(`games:${name}`, `queue`, '[]', 'max', maxMembers, (err, reply) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(reply)
+        })
+    })
+}
