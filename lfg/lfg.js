aa2f8bcc5f8cb21f92528d14254575299085c5a5
cthuluhoop123 <dowzhong@gmail.com>
2018-08-03 14:12:55 +1000

    Added bunch of stuff.

--- lfg/lfg.js
+++ lfg/lfg.js
@@ -2,37 +2,109 @@ const redis = require('redis')
 const client = redis.createClient()
 const lfg = module.exports
 
-lfg.getGames = () => {
-    return new Promise((resolve, reject) => {
-        client.scan('0', 'MATCH', 'games:*', (err, rep) => {
-            if (err) {
-                reject(err)
-            }
-            resolve(rep[1].map(game => game.replace(/games:/, '')))
-        })
-    })
-}
-
 lfg.addGame = (name, maxMembers) => {
     return new Promise((resolve, reject) => {
         client.hmset(`games:${name}`, `queue`, '[]', 'max', maxMembers, (err, reply) => {
             if (err) {
                 reject(err)
+                return
             }
             resolve(reply)
         })
     })
 }
 
-lfg._makeUniquePartyID = () => {
-    return new Promise((resolve, reject) => {
-        client.scan('0', 'MATCH', 'games:*:*', (err, rep) => {
+//i dont know where we should handle max members to a queue
+lfg.createGame = (game, mode, leaderMember) => {
+    return new Promise(async (resolve, reject) => {
+        let uniqueID = await this._makeUniquePartyID().catch(err => {
+            reject(err)
+        })
+        if (!uniqueID) return
+        client.hmset(`games:${game}:queues:${uniqueID}`, `leader`, JSON.stringify(leaderMember), `members`, JSON.stringify([leaderMember]), `mode`, mode, (err, reply) => {
             if (err) {
                 reject(err)
+                return
             }
-            resolve(rep[1].map(id => {
-                return id.replace(/^(games:[aA-zZ0-9]*:)([0-9]*)$/, '$2')
-            }))
+            resolve(uniqueID)
+        })
+    })
+}
+
+lfg.getGames = () => {
+    return new Promise((resolve, reject) => {
+        client.scan('0', 'MATCH', 'games:*', (err, rep) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(rep[1].map(game => game.replace(/games:/, '')))
+        })
+    })
+}
+
+lfg.listParties = (game = undefined) => {
+    return new Promise((resolve, reject) => {
+        client.scan('0', 'MATCH', `games:${game ? game : '*'}:queues:*`, async (err, reply) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            let arrayOfQueues = []
+            for (let queue of reply[1]) {
+                let queueInfo = await this._promiseHgetAll(queue)
+                if (queueInfo) {
+                    arrayOfQueues.push(Object.assign(queueInfo, { id: queue.replace(/^(games:[aA-zZ0-9]*:queues:)([0-9]*)$/, '$2')}))
+                }
+            }
+            resolve(arrayOfQueues)
+        })
+    })
+}
+
+lfg._makeUniquePartyID = () => {
+    return new Promise(async (resolve, reject) => {
+        let keepLooping = true
+        while (keepLooping) {
+            let uniqueID = Math.floor(100000 + Math.random() * 900000)
+            let duplicateIDs = await this._promiseScan('games:*:queues:*')
+                .catch(err => {
+                    reject(err)
+                })
+            if (!duplicateIDs) return
+            duplicateIDs = duplicateIDs[1].map(id => {
+                return id.replace(/^(games:[aA-zZ0-9]*:queues:)([0-9]*)$/, '$2')
+            }).filter(id => {
+                return id == uniqueID
+            })
+            if (duplicateIDs.length == 0) {
+                resolve(uniqueID)
+                keepLooping = false
+            }
+        }
+    })
+}
+
+lfg._promiseHgetAll = (search) => {
+    return new Promise((resolve, reject) => {
+        client.hgetall(search, (err, reply) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(reply)
+        })
+    })
+}
+
+lfg._promiseScan = (search) => {
+    return new Promise((resolve, reject) => {
+        client.scan('0', 'MATCH', search, (err, rep) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(rep)
         })
     })
 }
